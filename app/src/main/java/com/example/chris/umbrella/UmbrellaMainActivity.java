8c055da0e60f22de85ce2b17c3ec5d524ea7d05c
Chris Vansco <chris.vansco@gmail.com>
2017-12-02 21:02:05 -0500

    Current Temp and condition display properly.
    Changes color to warm or cold depending on the temp

--- app/src/main/java/com/example/chris/umbrella/UmbrellaMainActivity.java
+++ app/src/main/java/com/example/chris/umbrella/UmbrellaMainActivity.java
@@ -1,13 +1,21 @@
 package com.example.chris.umbrella;
 
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.design.widget.AppBarLayout;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.Toolbar;
 
 import com.example.chris.umbrella.di.umbrellamain.DaggerUmbrellaMainComponent;
+import com.example.chris.umbrella.model.Current.DisplayLocation;
 import com.example.chris.umbrella.model.Current.WeatherResponse;
 import com.example.chris.umbrella.model.Hourly.HourlyForecast;
 import com.example.chris.umbrella.model.Hourly.HourlyWeatherResponse;
@@ -24,18 +32,31 @@ public class UmbrellaMainActivity extends AppCompatActivity implements UmbrellaM
     
     @Inject
     UmbrellaMainPresenter presenter;
-    private List<HourlyForecast> hourlyForecasts;
     private String zip;
+    private List<HourlyForecast> hourlyForecasts;
+    private WeatherResponse currentWeather;
+    private TextView tvCurrentCondition;
+    private TextView tvCurrentTemp;
+    private SharedPreferences mSettings;
+    private android.support.v7.widget.Toolbar toolbar;
+    private DisplayLocation displayLocation;
+    private AppBarLayout appBarLayout;
     
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
         
         DaggerUmbrellaMainComponent.create().inject(this);
+        
+        tvCurrentTemp = findViewById(R.id.tvCurrentTemp);
+        tvCurrentCondition = findViewById(R.id.tvCurrentCondition);
+        toolbar = findViewById(R.id.toolbar);
     
+        appBarLayout = findViewById(R.id.appbar);
+        
+        
         zip = "07065";
         presenter.attachView(this);
     }
@@ -51,7 +72,7 @@ public class UmbrellaMainActivity extends AppCompatActivity implements UmbrellaM
     protected void onResume()
     {
         super.onResume();
-    
+        
         presenter.getHourlyWeather(zip);
         presenter.getCurrentWeather(zip);
     }
@@ -79,7 +100,23 @@ public class UmbrellaMainActivity extends AppCompatActivity implements UmbrellaM
     @Override
     public void setCurrentWeather(WeatherResponse weatherResponse)
     {
-        Log.d(TAG, "setCurrentWeather: "+weatherResponse);
+        currentWeather = weatherResponse;
+    
+        tvCurrentTemp.setText(currentWeather.getCurrentObservation().getTempF() + "°");
+        tvCurrentCondition.setText(currentWeather.getCurrentObservation().getWeather());
+        displayLocation = currentWeather.getCurrentObservation().getDisplayLocation();
+        getSupportActionBar().setTitle(displayLocation.getCity() + ", " + displayLocation.getState());
+    
+        if (Double.valueOf(currentWeather.getCurrentObservation().getTempF()) >= 60.0)
+        {
+            getSupportActionBar().setBackgroundDrawable(new ColorDrawable(ContextCompat.getColor(this,R.color.weather_warm)));
+            appBarLayout.setBackgroundColor(ContextCompat.getColor(this,R.color.weather_warm));
+        }
+        else
+        {
+            getSupportActionBar().setBackgroundDrawable(new ColorDrawable(ContextCompat.getColor(this,R.color.weather_cool)));
+            appBarLayout.setBackgroundColor(ContextCompat.getColor(this,R.color.weather_cool));
+        }
     }
     
     @Override
